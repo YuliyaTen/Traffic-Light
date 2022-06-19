54bd08c7b62efb94199e3f45fe9d49ebc89002da
Yuliya Ten <juliia.ten@gmail.com>
2022-06-19 16:23:10 +0300

    Setup

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var colorViews: [UIView]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for color in colorViews {
+            color.layer.cornerRadius = 70
+            color.alpha = 0.2
+        }
+        
+        startButton.layer.cornerRadius = 10
+
     }
 
-
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redView.alpha == yellowView.alpha {
+            redView.alpha = 100
+            greenView.alpha = 0.2
+        } else if yellowView.alpha == greenView.alpha {
+            redView.alpha = 0.2
+            yellowView.alpha = 100
+        } else if greenView.alpha == redView.alpha {
+            yellowView.alpha = 0.2
+            greenView.alpha = 100
+        } else if greenView.alpha == redView.alpha {
+            greenView.alpha = 0.2
+            redView.alpha = 100
+        }
+    }
+    
+    
 }
 
